80c6fc0efa42fdafcf369f4e362e4c01c4070985
Kirill Ivanov <roxiegreen@yandex.ru>
2024-04-16 22:09:54 +0300

    add first code for MySQL

---
+++ MySQL.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS HumanFriends;
+
+-- Создание таблицы (домашние животные)
+CREATE TABLE IF NOT EXISTS Pets (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255),
+    Type VARCHAR(50),
+    BirthDate DATE,
+    Commands VARCHAR(255)
+);
+
